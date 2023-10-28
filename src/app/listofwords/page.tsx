ccf30ce2bacd419438ab97572327a42d37bc5cd2
udaysarkarud <udayssarkarud@gmail.com>
2023-10-28 18:36:56 +0600

    fix: saved word show design

--- src/app/listofwords/page.tsx
+++ src/app/listofwords/page.tsx
@@ -1,3 +1,4 @@
+
 import Pagination from '@/components/Pagination/Pagination';
 import WordCard from '@/components/WordCard/WordCard';
 import WordSearch from '@/components/WordSearch/WordSearch';
@@ -6,7 +7,7 @@ import React from 'react';
 const listofwords = () => {
   return (
     <div>
-      <WordSearch title={'Search Your Word'} />
+      <WordSearch />
       <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 masonry">
         {/* cards */}
 
