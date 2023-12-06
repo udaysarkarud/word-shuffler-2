531284691f9956b9cccec8388c3e67f81de29799
udaysarkarud <udayssarkarud@gmail.com>
2023-12-06 14:22:29 +0600

    fix: change search result component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
+import WordCard from '@/components/WordCard/WordCard';
 import WordSearch from '@/components/WordSearch/WordSearch';
-import WordSearchResult from '@/components/WordSearchResult/WordSearchResult';
 
 export default function Home() {
   return (
     <>
       <div>
         <WordSearch />
-        <WordSearchResult />
+
+        <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 masonry">
+          {/* cards */}
+
+          <WordCard />
+          <WordCard />
+          <WordCard />
+          <WordCard />
+          <WordCard />
+          <WordCard />
+          <WordCard />
+          <WordCard />
+          <WordCard />
+          <WordCard />
+        </div>
       </div>
     </>
   );
